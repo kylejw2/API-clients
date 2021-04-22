261ac3f0c60352d93a7c0b5f7eeebdd98866c9c6
Kyle Wright <kylejamwright@gmail.com>
2021-04-22 16:04:31 -0600

    initial commit

--- README.md
+++ README.md
@@ -22,10 +22,15 @@ Each solution can be found on a feature branch within this repo. Here are the cu
 There are a few things that should be explored with each implementation in order for the test to be considered a success;
 
 - how does this solution handle state management/caching?
+- - Very well. React Query handles background fetching, inactive queries, and garbage collection. All of this is well documented [here](https://react-query.tanstack.com/guides/caching).
 - will this solution work when there are multiple client apps using it at the same time?
+- - Not sure. I couldn't find official documentation related to this question.
 - how does it handle "complicated" features? EG pagination, re-fetching, cache resolution
+- - Very well. Paginating is well-documented and includes options to keep already fetched data which makes the UI seem seamless. React Query handles auto refetching on specific window events. React Query also provides easy hooks to modify already triggering events or create new events that trigger a refetch. When it comes to a simple `refetch` method that can be called whenever we decide, React Query fails to provide it.
 - can we benefit from typescript types? EG codegen equivalent?
+- - Compatible with typescript. Briefly skimmed over [this](https://blog.logrocket.com/making-graphql-requests-easy-with-react-typescript-and-react-query/) so codegen seems possible.
 - can we easily configure subscription-like features? EG PubNub, GQL Subscriptions, WebSockets, etc?
+- - Not easily. React Query almost had a subscription hook but the founder said that they're no longer pursuing it. According to [his comments](https://github.com/tannerlinsley/react-query/issues/171), to have subscription-like behavior, we'll need to create that functionality ourselves.
 
 ## Project Outline
 
@@ -33,17 +38,13 @@ This project is a basic catalog for super mario games. As the test is not to bui
 
 For each implementation (for to meet all the requirements listed above), the following things should be built:
 
-- [ ] fetch paginated list of games
+- [x] fetch paginated list of games
 - [ ] fetch from cache when re-visiting page
 - [ ] mutate local cache with game updates
 - [ ] access cache from multiple views
 - [ ] manually update cache from non-api source
-- [ ] use typesafe hooks in a clean, simple API
+- [x] use typesafe hooks in a clean, simple API
 
-## GroundWork To Do
+## Personal Opinion
 
-- [ ] finish setting up typescript types
-- [ ] remove `details` route from navbar
-- [ ] add copy to homepage (and update the [designs](./WIREFRAME.png))
-- [ ] add details page UI
-- [ ] start create feature branch for each graphql framework using [this api](https://www.mariouniversalis.fr/graphql/)
+I've spent one whole day on React Query and I feel extremely familiar with it. It doesn't have a steep learning curve. The library works well with things that we need, such as codegen, typescript, paginated queries, and cache access and management. The built in devtools provide a nice touch to seeing the cache and knowing which queries are being managed, fetched, refetched, etc. The window event listeners are neat and would help some of our customers because I've heard that some have multiple sessions of the app open at once. Default React Query features would auto refetch data in the background of the application so our customers would be getting the most recent data without having to refresh their page.
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,141 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { request, gql } from 'graphql-request';
+
+const endpoint = 'https://www.mariouniversalis.fr/graphql/';
+
+const queryClient = new QueryClient();
 
 function App() {
+  const [gameId, setGameId] = React.useState(-1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <QueryClientProvider client={queryClient}>
+      <Games />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
+}
+
+interface GameType {
+  id: string | number;
+  name: string;
+  image: string;
+}
+
+function usePosts(page: number) {
+  return useQuery(
+    ['games', { page }],
+    async () => {
+      const { games } = await request(
+        endpoint,
+        gql`
+        query {
+          games(page: ${page}) {
+            pagination {
+              total_count
+              total_pages
+            }
+            data {
+              id
+              name
+              image
+            }
+          }
+        }
+      `
+      );
+      return games.data as GameType[];
+    },
+    { keepPreviousData: false }
+  );
+}
+const Game = ({ gameId }: { gameId: string }) => {
+  const [name, setName] = React.useState('');
+  const result = useQuery(
+    ['game', { page: 1 }, +gameId],
+    () => fetch('/games'),
+    {
+      initialData: () => {
+        return (queryClient as any)
+          .getQueryData('games')
+          ?.find((d: GameType) => d.id === gameId);
+      },
+    }
+  );
+  if (result.isError) return null;
+  if (result.isLoading) return null;
+
+  if (result.data.name && result.data.name !== name) {
+    setName(result.data.name);
+  }
+
+  return <div>Your game is: {name}</div>;
+};
+
+function Games() {
+  const [page, setPage] = React.useState(1);
+  const [gameId, setGameId] = React.useState('');
+  const { status, data, error, isFetching } = usePosts(
+    page
+  );
+  return (
+    <div style={{ padding: 30 }}>
+      <div style={{ display: 'flex' }}>
+        <div>
+          <h1>Locate a specific game</h1>
+          <input
+            value={gameId}
+            type='text'
+            onChange={(event) =>
+              setGameId(event.target.value)
+            }
+          />
+        </div>
+        <div>
+          {gameId ? <Game gameId={gameId} /> : null}
+        </div>
+      </div>
+      <h1>Games</h1>
+      <div>
+        {status === 'loading' ? (
+          'Loading...'
+        ) : status === 'error' ? (
+          <span>Error: {(error as any).message}</span>
+        ) : (
+          <>
+            <div>
+              {data &&
+                data.map((game) => (
+                  <div
+                    style={{ marginBottom: '20px' }}
+                    key={game.id}
+                  >
+                    <p style={{ marginBottom: 0 }}>
+                      ID: {game.id}
+                    </p>
+                    <p style={{ marginTop: 0 }}>
+                      Name: {game.name}
+                    </p>
+                    <img src={game.image} width='200' />
+                  </div>
+                ))}
+            </div>
+            <div>
+              {isFetching ? 'Background Updating...' : ' '}
+            </div>
+          </>
+        )}
+      </div>
+      <button onClick={() => setPage((prev) => prev + 1)}>
+        Load more
+      </button>
     </div>
   );
 }
